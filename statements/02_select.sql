245db8233477387270d6212b1c07c17400c3389f
el_loco <alejandrobolivarechverri@gmail.com>
2026-01-17 18:50:06 -0500

    visto de la clase 1 a la 5

---
+++ statements/02_select.sql
@@ -0,0 +1,2 @@
+SELECT * FROM users;
+SELECT users_id, name FROM users;
---
+++ statements/03_where.sql
@@ -0,0 +1,5 @@
+SELECT * FROM users WHERE age = 16;
+
+SELECT users FROM users WHERE age = 16;
+
+SELECT DISTINCT age FROM users WHERE age = 16;
